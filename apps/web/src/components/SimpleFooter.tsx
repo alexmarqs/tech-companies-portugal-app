084d734cb16a32468e8f09e455ab9ad2602d6bd0
Alexandre Marques <pt.alexandremarques@gmail.com>
2025-03-21 18:41:51 +0000

    style: adjust padding in SimpleFooter for improved layout

--- apps/web/src/components/SimpleFooter.tsx
+++ apps/web/src/components/SimpleFooter.tsx
@@ -3,7 +3,7 @@ import { LogoFooter } from "./LogoFooter";
 export default function SimpleFooter() {
   return (
     <footer className="bg-background font-mono">
-      <div className="container flex flex-col gap-4 p-6">
+      <div className="container flex flex-col gap-4 p-3">
         <div className="w-full flex flex-col md:flex-row md:justify-between items-center flex-wrap gap-4">
           <LogoFooter />
           <p className="text-xs text-center">
