9d0fd703f3bb44538666c6f07217ceb993ffc594
Alexandre Marques <pt.alexandremarques@gmail.com>
2025-08-07 15:45:08 +0200

    refactor: clean up imports and remove unused components in layout and form files

--- apps/web/src/app/location/[location]/layout.tsx
+++ apps/web/src/app/location/[location]/layout.tsx
@@ -1,7 +1,5 @@
 import Footer from "@/components/Footer";
-import { Skeleton } from "@/components/ui/skeleton";
 import type { LayoutProps } from "@/lib/types";
-import { Suspense } from "react";
 
 export default function LocationPageLayout({ children }: LayoutProps) {
   return (
